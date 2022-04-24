24393aadfffbf821cccf0d2a13e429b361e7a240
Olivier Van hamme <nicefiction@gmail.com>
2022-04-24 09:48:21 +0200

    Adds a View and Model folder to the project. Rearranges the files in the project, and creates code templates.

--- DatePlanner/DatePlannerApp.swift
+++ DatePlanner/DatePlannerApp.swift
@@ -1,17 +1,65 @@
-//
-//  DatePlannerApp.swift
-//  DatePlanner
-//
-//  Created by Olivier Van hamme on 24/04/2022.
-//
-
+// MARK: - LIBRARIES
 import SwiftUI
+
+
 
 @main
 struct DatePlannerApp: App {
+    
+    // MARK: - STATIC PROPERTIES
+    // MARK: - PROPERTY WRAPPERS
+    // MARK: - PROPERTIES
+    // MARK: - COMPUTED PROPERTIES
     var body: some Scene {
+        
         WindowGroup {
+            
             ContentView()
         }
     }
+    
+    
+    
+    // MARK: - STATIC METHODS
+    // MARK: - INITIALIZERS
+    // MARK: - METHODS
+    // MARK: - HELPER METHODS
 }
+
+
+
+
+
+/*
+ // MARK: - STATIC PROPERTIES
+ // MARK: - PROPERTY WRAPPERS
+ // MARK: - PROPERTIES
+ // MARK: - COMPUTED PROPERTIES
+ var body: some View {
+ 
+     Text("Hello, world!")
+ }
+ 
+ 
+ 
+ // MARK: - STATIC METHODS
+ // MARK: - INITIALIZERS
+ // MARK: - METHODS
+ // MARK: - HELPER METHODS
+ */
+/*
+ // MARK: - STATIC PROPERTIES
+ // MARK: - PROPERTY WRAPPERS
+ // MARK: - PROPERTIES
+ // MARK: - COMPUTED PROPERTIES
+ // MARK: - STATIC METHODS
+ // MARK: - INITIALIZERS
+ // MARK: - METHODS
+ // MARK: - HELPER METHODS
+ */
+/*
+ // PREVIEW ////////////////////////////////////
+ 
+ // MARK: - STATIC PROPERTIES
+ // MARK: - COMPUTED PROPERTIES
+ */
