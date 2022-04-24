b03027e18af5a4d06907286652d74b309ff9f4e7
Olivier Van hamme <nicefiction@gmail.com>
2022-04-24 09:42:41 +0200

    Initial Commit

---
+++ DatePlanner/DatePlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatePlannerApp.swift
+//  DatePlanner
+//
+//  Created by Olivier Van hamme on 24/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct DatePlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
